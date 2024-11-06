70aa80326937de5b458d5cb66fdc6fb864aeedf8
Ensong Wei <61647484+touero@users.noreply.github.com>
2024-11-06 16:30:32 +0800

    fix: update fonts.lua

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,16 +1,17 @@
 local wezterm = require('wezterm')
 
 return {
-   font = wezterm.font_with_fallback({
-      { family = 'JetBrainsMono Nerd Font', weight = 'Regular' },
-      { family = 'JetBrains Mono' },
-      { family = 'FiraCode Nerd Font' },
-      { family = 'Noto Sans SC:style=Regular' },
-      { family = 'Noto Sans Mono CJK SC' },
-   }),
-   font_size = 11,
+    font = wezterm.font_with_fallback({
+        { family = 'Monaco' },
+        { family = 'JetBrainsMono Nerd Font', weight = 'Regular' },
+        { family = 'JetBrains Mono' },
+        { family = 'FiraCode Nerd Font' },
+        { family = 'Noto Sans SC:style=Regular' },
+        { family = 'Noto Sans Mono CJK SC' },
+    }),
+    font_size = 11,
 
-   --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
-   freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
-   freetype_render_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
+    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
+    freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
+    freetype_render_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
 }
