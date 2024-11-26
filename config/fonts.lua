1a6e1fcc6684f847b2983565e94913b5efd784c3
Ensong Wei <61647484+touero@users.noreply.github.com>
2024-11-26 14:23:23 +0800

    fix: default font size to 13

--- config/fonts.lua
+++ config/fonts.lua
@@ -9,7 +9,7 @@ return {
         { family = 'Noto Sans SC:style=Regular' },
         { family = 'Noto Sans Mono CJK SC' },
     }),
-    font_size = 11,
+    font_size = 13,
 
     --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
     freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
